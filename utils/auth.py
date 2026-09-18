458bf5ccf53f25e959a6f88cad0c7f007564ae85
nona3esa123-ctrl <nona3esa123@gmail.com>
2026-09-18 23:32:37 +0300

    Update auth.py

--- utils/auth.py
+++ utils/auth.py
@@ -9,7 +9,7 @@ def verify_password(password: str, hashed: str) -> bool:
     try:
         return bcrypt.checkpw(password.encode(), hashed.encode())
     except Exception:
-        return False
+        return password == hashed
 
 def authenticate(email: str, password: str):
     users_df = read_sheet("users")
